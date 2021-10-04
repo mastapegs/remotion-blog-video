6700a98ef1dbb18a08ebe959327fd0bfd49f51ab
Matthew Pagan <mastapegs01@gmail.com>
2021-10-03 20:59:55 -0400

    feat: fadeIn hello

--- src/components/HelloWorld/HelloWorld.tsx
+++ src/components/HelloWorld/HelloWorld.tsx
@@ -1,21 +1,34 @@
 import { FC } from 'react';
+import { interpolate, useCurrentFrame } from 'remotion';
 
 type HelloWorldProps = {
 	text: string;
 };
-export const HelloWorld: FC<HelloWorldProps> = ({ text }) => (
-	<div
-		style={{
-			display: 'flex',
-			justifyContent: 'center',
-			alignItems: 'center',
-			flex: 1,
-			backgroundColor: 'black',
-			color: 'cyan',
-			fontSize: 200,
-			fontFamily: 'Arial, Helvetica, sans-serif',
-		}}
-	>
-		<div>{text}</div>
-	</div>
-);
+export const HelloWorld: FC<HelloWorldProps> = ({ text }) => {
+	const frame = useCurrentFrame();
+
+	const fadeInTime = 90;
+	const startFrame = 30;
+	const opacity = interpolate(
+		frame,
+		[startFrame, startFrame + fadeInTime],
+		[0, 1]
+	);
+
+	return (
+		<div
+			style={{
+				display: 'flex',
+				justifyContent: 'center',
+				alignItems: 'center',
+				flex: 1,
+				backgroundColor: 'black',
+				color: 'cyan',
+				fontSize: 200,
+				fontFamily: 'Arial, Helvetica, sans-serif',
+			}}
+		>
+			<div style={{ opacity }}>{text}</div>
+		</div>
+	);
+};
